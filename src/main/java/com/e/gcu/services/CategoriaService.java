e6cbc8e3dde3d9414db2790401847794496174d9
guilhermeuberti <guilherme.uberti@hotmail.com>
2019-09-19 14:59:07 -0300

    POST, PUT e DELETE para Categoria

--- src/main/java/com/e/gcu/services/CategoriaService.java
+++ src/main/java/com/e/gcu/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.e.gcu.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.e.gcu.domain.Categoria;
 import com.e.gcu.repositories.CategoriaRepository;
+import com.e.gcu.services.exceptions.DataIntegrityException;
 
 @Service
 public class CategoriaService {
@@ -23,5 +25,20 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		try {
+			find(id);
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possua Produtos!");
+		}
 	}
 }
